06254c78c82fff043968c634d19c91fb1bc80b82
Ben Hagen <c.ben.hagen@gmail.com>
2016-09-30 14:40:57 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="turboactivate",
-      version="1.0.4",
+      version="1.0.5",
       description="Python bindings for TurboActivate",
       url="https://github.com/develersrl/python-turboactivate/",
       author="Develer S.r.L",
