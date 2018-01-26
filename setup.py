d9bc71e3baeaa78906f5b90c51b8724f432d900b
Lorenzo Villani <lvillani@develer.com>
2018-01-26 11:15:59 +0100

    setup.py: version 4 and stable

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="turboactivate",
-      version="2.0.0",
+      version="4.0.0",
       description="Python bindings for TurboActivate",
       url="https://github.com/develersrl/python-turboactivate/",
       author="Develer S.r.L",
@@ -11,7 +11,7 @@ setup(name="turboactivate",
       maintainer="Lorenzo Villani",
       maintainer_email="lvillani@develer.com",
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Topic :: Software Development :: Libraries :: Python Modules',
